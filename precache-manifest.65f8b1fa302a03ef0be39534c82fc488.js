da911d81a3da0ccb70d00ef556deba3f5721c1d5
rohilkhadgee <rohilkhadgee@gmail.com>
2020-10-24 20:35:12 +1100

    Updates

---
+++ precache-manifest.65f8b1fa302a03ef0be39534c82fc488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a9268de2243151eb62b3d509538303",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f93a4a2a25e9521b3129",
+    "url": "/monsters-rolodex/static/css/main.e28aa188.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93a4a2a25e9521b3129",
+    "url": "/monsters-rolodex/static/js/main.3a6e16e5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
